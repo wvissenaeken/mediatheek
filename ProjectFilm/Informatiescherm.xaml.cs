eafaaffac30ef02c95d9d9cd95fdb1a621ef85a7
Mathieu <mathieurosseeuw@hotmail.com>
2016-12-26 15:48:23 +0100

    code zover

---
+++ ProjectFilm/Informatiescherm.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectFilm
+{
+    /// <summary>
+    /// Interaction logic for Informatiescherm.xaml
+    /// </summary>
+    public partial class Informatiescherm : Window
+    {
+        public Informatiescherm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKoop_Click(object sender, RoutedEventArgs e)
+        {
+          Close();
+        }
+    }
+}
---
+++ ProjectFilm/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectFilm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKoop_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Komt binnenkort!", "", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void btnHuur_Click(object sender, RoutedEventArgs e)
+        {
+            Huurscherm huurverwijzing = new Huurscherm();
+            huurverwijzing.ShowDialog();
+        }
+
+        private void btnMeerInfo_Click(object sender, RoutedEventArgs e)
+        {
+            Informatiescherm informatieverwijzing = new Informatiescherm();
+            informatieverwijzing.ShowDialog();
+        }
+    }
+}
