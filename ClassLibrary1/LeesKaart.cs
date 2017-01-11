9f09ffa1a4ce1405ea162e96e45b1554c02eaa9d
William <williamvissenaeken@gmail.com>
2017-01-11 14:31:53 +0100

    Herschikken LeesKaart.cs

--- ClassLibrary1/LeesKaart.cs
+++ ClassLibrary1/LeesKaart.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using Net.Sf.Pkcs11;
-using Net.Sf.Pkcs11.Objects;
-using Net.Sf.Pkcs11.Wrapper;
-using System.Drawing;
 using System.IO;
 using System.Windows.Media.Imaging;
 using System.Windows.Media;
+using Net.Sf.Pkcs11;
+using Net.Sf.Pkcs11.Objects;
+using Net.Sf.Pkcs11.Wrapper;
 
 namespace ProjectFilmLibrary
 {
@@ -14,12 +12,10 @@ namespace ProjectFilmLibrary
     {
         private Module m = null;
         private String mFileName;
-        /// <summary>
-        /// Default constructor. Will instantiate the beidpkcs11.dll pkcs11 module
-        /// </summary>
+        //Initialiseer beidpkcs11.dll pkcs11 module!
         public LeesKaart()
         {
-            mFileName = "beidpkcs11.dll";
+            mFileName = "beidpkcs11.dll"; //Eid-middelware dient wel geïnstalleerd te zijn op client!
         }
         public LeesKaart(String moduleFileName)
         {
@@ -33,12 +29,8 @@ namespace ProjectFilmLibrary
             {
                 m = Module.GetInstance(mFileName);
             }
-            //initialization now occurs within the getinstance function
-            //m.Initialize();
             try
             {
-                // Look for slots (cardreaders)
-                // GetSlotList(false) will return all cardreaders
                 Slot[] slots = m.GetSlotList(false);
                 if (slots.Length == 0)
                     slotID = "";
@@ -47,7 +39,6 @@ namespace ProjectFilmLibrary
             }
             finally
             {
-                //m.Finalize_();
                 m.Dispose();
                 m = null;
             }
@@ -61,12 +52,8 @@ namespace ProjectFilmLibrary
             {
                 m = Module.GetInstance(mFileName);
             }
-            //m.Initialize();
             try
             {
-                // Look for slots (cardreaders)
-                // GetSlotList(true) will return only the cardreaders with a 
-                // token (smart card)
                 tokenInfoLabel = m.GetSlotList(true)[0].Token.TokenInfo.Label.Trim();
             }
             finally
@@ -78,6 +65,113 @@ namespace ProjectFilmLibrary
 
         }
 
+        //Methode voor oproepen van betrokken data
+        public string GetData(String label)
+        {
+            String value = "";
+            if (m == null)
+            {
+                m = Module.GetInstance(mFileName);
+            }
+            // Initialiseer pkcs11 
+            try
+            {
+                Slot[] slotlist = m.GetSlotList(true);
+                if (slotlist.Length > 0)
+                {
+                    Slot slot = slotlist[0];
+
+                    Session session = slot.Token.OpenSession(true);
+
+                    ByteArrayAttribute classAttribute = new ByteArrayAttribute(CKA.CLASS);
+                    classAttribute.Value = BitConverter.GetBytes((uint)Net.Sf.Pkcs11.Wrapper.CKO.DATA);
+
+                    ByteArrayAttribute labelAttribute = new ByteArrayAttribute(CKA.LABEL);
+                    labelAttribute.Value = System.Text.Encoding.UTF8.GetBytes(label);
+
+                    session.FindObjectsInit(new P11Attribute[] { classAttribute, labelAttribute });
+                    P11Object[] foundObjects = session.FindObjects(50);
+                    int counter = foundObjects.Length;
+                    Data data;
+                    while (counter > 0)
+                    {
+                        data = foundObjects[counter - 1] as Data;
+                        label = data.Label.ToString();
+                        if (label != null)
+                            Console.WriteLine(label);
+                        if (data.Value.Value != null)
+                        {
+                            value = System.Text.Encoding.UTF8.GetString(data.Value.Value);
+                            Console.WriteLine(value);
+                        }
+                        counter--;
+                    }
+                    session.FindObjectsFinal();
+                    session.Dispose();
+                }
+                else
+                {
+                    Console.WriteLine("Geen kaart gevonden!\n");
+                }
+            }
+            finally
+            {
+                // Beëindig pkcs11 
+                m.Dispose();
+                m = null;
+            }
+            return value;
+        }
+
+        //Methode voor oproepen betrokken bestanden (foto)
+        private byte[] GetFile(String Filename)
+        {
+            byte[] value = null;
+
+            // Initialiseer pkcs11 
+            if (m == null)
+            {
+                m = Module.GetInstance(mFileName);
+            }
+
+            try
+            {
+                Slot[] slotlist = m.GetSlotList(true);
+                if (slotlist.Length > 0)
+                {
+                    Slot slot = slotlist[0];
+                    Session session = slot.Token.OpenSession(true);
+      
+                    ByteArrayAttribute fileLabel = new ByteArrayAttribute(CKA.LABEL);
+                    fileLabel.Value = System.Text.Encoding.UTF8.GetBytes(Filename);
+                    ByteArrayAttribute fileData = new ByteArrayAttribute(CKA.CLASS);
+                    fileData.Value = BitConverter.GetBytes((uint)Net.Sf.Pkcs11.Wrapper.CKO.DATA);
+                    session.FindObjectsInit(new P11Attribute[] {
+                        fileLabel,fileData
+                    });
+                    P11Object[] foundObjects = session.FindObjects(1);
+                    if (foundObjects.Length != 0)
+                    {
+                        Data file = foundObjects[0] as Data;
+                        value = file.Value.Value;
+                    }
+                    session.FindObjectsFinal();
+                }
+                else
+                {
+                    Console.WriteLine("No card found\n");
+                }
+            }
+            finally
+            {
+                // Beëindig pkcs11 
+                m.Dispose();
+                m = null;
+            }
+            return value;
+        }
+
+        //Pluk de nodige waarden
         public string GetSurname()
         {
             return GetData("surname");
@@ -111,6 +205,7 @@ namespace ProjectFilmLibrary
             return GetData("card_number");
         }
 
+        //Converteer Bitmap naar Image
         public ImageSource GetPhotoFile()
         {
             var photoFile = GetFile("PHOTO_FILE");
@@ -125,126 +220,6 @@ namespace ProjectFilmLibrary
             }
 
             return photo;
-        }
-
-        //Generische functie voor oproepen van betrokken data
-        //Bron:Eid SDK
-        public string GetData(String label)
-        {
-            String value = "";
-            if (m == null)
-            {
-                m = Module.GetInstance(mFileName);
-            }
-            // pkcs11 module init
-            //m.Initialize();
-            try
-            {
-                // Get the first slot (cardreader) with a token
-                Slot[] slotlist = m.GetSlotList(true);
-                if (slotlist.Length > 0)
-                {
-                    Slot slot = slotlist[0];
-
-                    Session session = slot.Token.OpenSession(true);
-
-                    // Search for objects
-                    // First, define a search template 
-
-                    // "The label attribute of the objects should equal ..."
-                    ByteArrayAttribute classAttribute = new ByteArrayAttribute(CKA.CLASS);
-                    classAttribute.Value = BitConverter.GetBytes((uint)Net.Sf.Pkcs11.Wrapper.CKO.DATA);
-
-
-                    ByteArrayAttribute labelAttribute = new ByteArrayAttribute(CKA.LABEL);
-                    labelAttribute.Value = System.Text.Encoding.UTF8.GetBytes(label);
-
-
-                    session.FindObjectsInit(new P11Attribute[] { classAttribute, labelAttribute });
-                    P11Object[] foundObjects = session.FindObjects(50);
-                    int counter = foundObjects.Length;
-                    Data data;
-                    while (counter > 0)
-                    {
-                        //foundObjects[counter-1].ReadAttributes(session);
-                        //public static BooleanAttribute ReadAttribute(Session session, uint hObj, BooleanAttribute attr)
-                        data = foundObjects[counter - 1] as Data;
-                        label = data.Label.ToString();
-                        if (label != null)
-                            Console.WriteLine(label);
-                        if (data.Value.Value != null)
-                        {
-                            value = System.Text.Encoding.UTF8.GetString(data.Value.Value);
-                            Console.WriteLine(value);
-                        }
-                        counter--;
-                    }
-                    session.FindObjectsFinal();
-                    session.Dispose();
-                }
-                else
-                {
-                    Console.WriteLine("Geen kaart gevonden!\n");
-                }
-            }
-            finally
-            {
-                // pkcs11 finalize
-                m.Dispose();//m.Finalize_();
-                m = null;
-            }
-            return value;
-        }
-
-        private byte[] GetFile(String Filename)
-        {
-            byte[] value = null;
-            // pkcs11 module init
-            if (m == null)
-            {
-                m = Module.GetInstance(mFileName);
-            }
-            //m.Initialize();
-            try
-            {
-                // Get the first slot (cardreader) with a token
-                Slot[] slotlist = m.GetSlotList(true);
-                if (slotlist.Length > 0)
-                {
-                    Slot slot = slotlist[0];
-                    Session session = slot.Token.OpenSession(true);
-
-                    // Search for objects
-                    // First, define a search template 
-
-                    // "The label attribute of the objects should equal ..."                
-                    ByteArrayAttribute fileLabel = new ByteArrayAttribute(CKA.LABEL);
-                    fileLabel.Value = System.Text.Encoding.UTF8.GetBytes(Filename);
-                    ByteArrayAttribute fileData = new ByteArrayAttribute(CKA.CLASS);
-                    fileData.Value = BitConverter.GetBytes((uint)Net.Sf.Pkcs11.Wrapper.CKO.DATA);
-                    session.FindObjectsInit(new P11Attribute[] {
-                        fileLabel,fileData
-                    });
-                    P11Object[] foundObjects = session.FindObjects(1);
-                    if (foundObjects.Length != 0)
-                    {
-                        Data file = foundObjects[0] as Data;
-                        value = file.Value.Value;
-                    }
-                    session.FindObjectsFinal();
-                }
-                else
-                {
-                    Console.WriteLine("No card found\n");
-                }
-            }
-            finally
-            {
-                // pkcs11 finalize
-                m.Dispose();//m.Finalize_();
-                m = null;
-            }
-            return value;
         }
     }
 }
