a02b7af94a005d6205307e69e4a75b17249f5a8d
William <williamvissenaeken@gmail.com>
2017-01-12 23:00:16 +0100

    Geldwisselaar optimalisering

--- ProjectFilm/Cashscherm.xaml.cs
+++ ProjectFilm/Cashscherm.xaml.cs
@@ -25,7 +25,7 @@ namespace ProjectFilm
         //Declareer nodige variabelen
         public decimal TotaalInworp;
         public decimal Wisselgeld;
-        public int Teller100, Teller50;
+        public int Teller100, Teller50, Teller20, Teller10, Teller5, Teller2, Teller1, Teller50C, Teller20C, Teller10C, Teller5C;
 
         public IEnumerable<Control> Controls { get; private set; }
 
@@ -34,16 +34,12 @@ namespace ProjectFilm
         {
             TotaalInworp += Inworp;
             lblHuidigeInworp.Content = String.Format("€ {0}", TotaalInworp);
-            //Zie methode lager
-            //VerifieerTotaalInworp();
         }
 
         public void GeldMinClick(decimal Inworp)
         {
             TotaalInworp -= Inworp;
             lblHuidigeInworp.Content = String.Format("€ {0}", TotaalInworp);
-            //Zie methode lager
-            //VerifieerTotaalInworp();
         }
 
         //Enkel click event voor alle knoppen mbt Geld
@@ -60,32 +56,37 @@ namespace ProjectFilm
                     GeldPlusClick(50.00M);
                     lbl50_teller.Content = (Teller50 += 1).ToString();
                     break;
-                case "20":
+                case "btn20_plus":
                     GeldPlusClick(20.00M);
+                    lbl20_teller.Content = (Teller20 += 1).ToString();
                     break;
-                case "10":
+                case "btn10_plus":
                     GeldPlusClick(10.00M);
+                    lbl10_teller.Content = (Teller10 += 1).ToString();
                     break;
-                case "5":
+                case "btn5_plus":
                     GeldPlusClick(5.00M);
+                    lbl5_teller.Content = (Teller5 += 1).ToString();
                     break;
-                case "2":
+                case "btn2_plus":
                     GeldPlusClick(2.00M);
+                    lbl2_teller.Content = (Teller2 += 1).ToString();
                     break;
-                case "1":
+                case "btn1_plus":
                     GeldPlusClick(1.00M);
+                    lbl1_teller.Content = (Teller1 += 1).ToString();
                     break;
-                case "0,50":
+                case "btn50C_plus":
                     GeldPlusClick(0.50M);
+                    lbl50C_teller.Content = (Teller50C += 1).ToString();
                     break;
-                case "0,20":
+                case "btn20C_plus":
                     GeldPlusClick(0.20M);
+                    lbl20C_teller.Content = (Teller20C += 1).ToString();
                     break;
-                case "0,10":
+                case "btn10C_plus":
                     GeldPlusClick(0.10M);
-                    break;
-                case "0,05":
-                    GeldPlusClick(0.05M);
+                    lbl10C_teller.Content = (Teller10C += 1).ToString();
                     break;
             }
         }
@@ -94,17 +95,50 @@ namespace ProjectFilm
         {
             Button btn = sender as Button;
             //Teller kan en mag niet kleiner zijn dan nul!
-            if (Teller100 > 0 || Teller50 > 0)
+            if (Teller100 > 0 || Teller50 > 0 || Teller20 > 0 || Teller10 > 0 || Teller5 > 0 || 
+                Teller2 > 0 || Teller1 > 0 || Teller50C > 0 || Teller20C > 0 || Teller10C > 0 )
             {
                 switch (btn.Name.ToString())
                 {
                     case "btn100_min":
-                        GeldPlusClick(-100.00M);
+                        GeldMinClick(100.00M);
                         lbl100_teller.Content = (Teller100 -= 1).ToString();
                         break;
                     case "btn50_min":
-                        GeldPlusClick(-50.00M);
+                        GeldMinClick(50.00M);
                         lbl50_teller.Content = (Teller50 -= 1).ToString();
+                        break;
+                    case "btn20_min":
+                        GeldPlusClick(20.00M);
+                        lbl20_teller.Content = (Teller20 -= 1).ToString();
+                        break;
+                    case "btn10_min":
+                        GeldMinClick(10.00M);
+                        lbl10_teller.Content = (Teller10 -= 1).ToString();
+                        break;
+                    case "btn5_min":
+                        GeldMinClick(5.00M);
+                        lbl5_teller.Content = (Teller5 -= 1).ToString();
+                        break;
+                    case "btn2_min":
+                        GeldMinClick(2.00M);
+                        lbl2_teller.Content = (Teller2 -= 1).ToString();
+                        break;
+                    case "btn1_min":
+                        GeldMinClick(1.00M);
+                        lbl1_teller.Content = (Teller1 -= 1).ToString();
+                        break;
+                    case "btn50C_min":
+                        GeldMinClick(0.50M);
+                        lbl50C_teller.Content = (Teller50C -= 1).ToString();
+                        break;
+                    case "btn20C_min":
+                        GeldMinClick(0.20M);
+                        lbl20C_teller.Content = (Teller20C -= 1).ToString();
+                        break;
+                    case "btn10C_min":
+                        GeldMinClick(0.10M);
+                        lbl10C_teller.Content = (Teller10C -= 1).ToString();
                         break;
                 }
             }
